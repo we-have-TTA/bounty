27ccdf66578768e3d55d1cef80fc7863c7ff0f71
noracami <kerke.2011@gmail.com>
2022-08-09 15:43:58 +0800

    disable unuse route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # root to: "posts#index"
   root "pages#index"
-  resources :comments
-  resources :posts
-  resources :users
+  # resources :comments, except: %i[ create destroy]
+  resources :posts, only: %i[ new create show ] do
+    resources :comments, shallow: true, only: %i[ create destroy]
+  end
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
